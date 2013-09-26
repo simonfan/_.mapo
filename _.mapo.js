115c05774957b08baf0b8b5f03a2cd5e6608a01d
simonfan <simonxd@gmail.com>
2013-09-26 08:25:48 -0300

    maps aarrays into objects now

--- _.mapo.js
+++ _.mapo.js
@@ -1,11 +1,19 @@
 define(['underscore'], function(undef) {
+
+	function iterArray(res, iter, value, name) {
+		res[ value ] = iter(value, name);
+	}
+
+	function iterObject(res, iter, value, name) {
+		res[ name ] = iter(value, name);
+	}
+
 	_.mixin({
 		mapo: function(obj, iter) {
-			var res = {};
+			var res = {},
+				_iter = _.isArray(obj) ? iterArray : iterObject;
 			
-			_.each(obj, function(value, name) {
-				res[ name ] = iter(value, name);
-			});
+			_.each(obj, _.partial(_iter, res, iter));
 
 			return res;
 		}
